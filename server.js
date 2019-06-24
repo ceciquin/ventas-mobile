db5bcbad839ff25ff45bd7954c0589911ba48de6
Quintana Amarilla <ceciquin@protonmail.ch>
2019-06-23 22:34:58 -0300

    Agregue un template HTML y EJS como "view engine"

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express'); // indicate module requere for express
-var chalk = require('chalk'); // give color to certain message at cmd
-var debug = require('debug')('server'); // give the option to debug only in debug mode
-var path = require('path'); // to get the "join" method, to join strings
-var morgan = require('morgan');//information about web traffic
-var bodyParser = require('body-parser');
+const express = require('express'); // indicate module requere for express
+const chalk = require('chalk'); // give color to certain message at cmd
+const debug = require('debug')('server'); // give the option to debug only in debug mode
+const path = require('path'); // to get the "join" method, to join strings
+const morgan = require('morgan');//information about web traffic
+const bodyParser = require('body-parser');
 
-var server = express(); //instance of express
+const server = express(); //instance of express
 
 server.use(morgan('tiny')); //call Morgan utility
 
@@ -13,11 +13,16 @@ server.use('/css',express.static(path.join(__dirname,'/public/')));//reference t
 server.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css/'))); //reference to static files downloaded as node modules.-CSS
 server.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js/')));//Reference for js files - javascript
 server.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/')));//reference for jquery functionality
+server.set('views', './src/view'); // dir for the engine views
+server.set ('view engine', 'ejs');
 
-server.get('/',function(req,res){
-    res.sendfile(path.join(__dirname,'/views/index.html'));// current location of the file
+
+server.get('/',(req, res) =>{ //get Method route
+    res.render('index',{title: 'Ventas Mobile'});
 });
+    
+    
 
-server.listen(3000, function(){
+server.listen(3000,() => {
     debug(`listening on port ${chalk.green('3000')}`);
 });
