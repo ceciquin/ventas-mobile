c635ed9147c56ea3c602a02653cbf6357c88a274
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2019-07-08 19:23:09 -0300

    add endpoint

--- server.js
+++ server.js
@@ -3,26 +3,44 @@ const chalk = require('chalk'); // give color to certain message at cmd
 const debug = require('debug')('server'); // give the option to debug only in debug mode
 const path = require('path'); // to get the "join" method, to join strings
 const morgan = require('morgan');//information about web traffic
-const bodyParser = require('body-parser');
+//const _ = require('lodash');
 
 const server = express(); //instance of express
 
 server.use(morgan('tiny')); //call Morgan utility
 
-server.use('/css',express.static(path.join(__dirname,'/public/')));//reference to static components in "public" folder
-server.use('/css',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/css/'))); //reference to static files downloaded as node modules.-CSS
-server.use('/js',express.static(path.join(__dirname,'/node_modules/bootstrap/dist/js/')));//Reference for js files - javascript
-server.use('/js',express.static(path.join(__dirname,'/node_modules/jquery/')));//reference for jquery functionality
+server.use('/css', express.static(path.join(__dirname, '/public/')));//reference to static components in "public" folder
+server.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/'))); //reference to static files downloaded as node modules.-CSS
+server.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js/')));//Reference for js files - javascript
+server.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/')));//reference for jquery functionality
 server.set('views', './src/view'); // dir for the engine views
-server.set ('view engine', 'ejs');
+server.set('view engine', 'ejs');
 
 
-server.get('/',(req, res) =>{ //get Method route
-    res.render('index',{title: 'Ventas Mobile'});
+server.get('/', (req, res) => { //get Method route
+    res.render('index', {title: 'Ventas Mobile'});
 });
-    
-    
 
-server.listen(3000,() => {
+// ###############    ORDERS    ###############
+
+server.get('/orders', (req, res) => {
+    //let products = db_client.products.getAll();
+    res.render('ordersForm', {products: [{name: "yerba"}]});
+});
+
+server.post('orders/new_order', (res, req) => {
+    let queryString = req.query;
+
+    /*db_client.order.save(queryString, function (err) {
+        if (err) res.json({status: 500, message: "There was an error trying to save the order"})
+    });*/
+
+    // if everyting ok, then returns to the orders form to create a new one
+    //res.render('ordersForm', {products: db_client.products.getAll()});
+});
+
+// ###############    ORDERS    ###############
+
+server.listen(3000, () => {
     debug(`listening on port ${chalk.green('3000')}`);
 });
