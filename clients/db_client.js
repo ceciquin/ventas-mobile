b1f324a72a64284bbe6f9ada6f3b58dcde6c422f
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2020-02-26 13:22:50 -0300

    some stuff I cannot remember, lol

--- clients/db_client.js
+++ clients/db_client.js
@@ -35,13 +35,25 @@ function getProducts() {
 }
 
 function newOrder(params) {
+    return executeQuery('select nro_Pedido from Pedido')
+                .then(function (rs) {
+                    let orderId = Number(rs.recordset[(rs.recordset.length - 1)].nro_Pedido);
 
+                    let orderValues = [params.client, params.seller, params.products[0].id];
+                    executeQuery('insert into Pedido(nro_Cliente, nro_Vendedor, id_Producto) values(' + orderValues.join(',') + ')');
+
+                    params.products.forEach(function (p) {
+                        let valuesProducts = [(p.id.toString() + orderId.toString()), p.id, orderId, p.qty];
+                        executeQuery('insert into ItemPedido (id_itemProducto, id_Producto, id_Pedido, cantidad) values (' + valuesProducts.join(',') + ')')
+                    });
+                })
+                .then(function () { return { status: 200, message: "New order saved successfuly" } });
 }
 
 function getStock() {
     return executeQuery('select p.Nombre_Producto, s.cantidadDisponible_Producto from Producto as p join Stock as s on p.id_Producto = s.id_Producto')
-        .then((rs) => { 
-            return _.map(rs.recordset, function(register) {
+        .then((rs) => {
+            return _.map(rs.recordset, function (register) {
                 let product = {};
 
                 product.name = register.Nombre_Producto;
@@ -54,5 +66,6 @@ function getStock() {
 
 module.exports = {
     getProducts: getProducts,
-    getStock: getStock
+    getStock: getStock,
+    newOrder: newOrder
 }
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const chalk = require('chalk'); // give color to certain message at cmd
 const debug = require('debug')('server'); // give the option to debug only in debug mode
 const path = require('path'); // to get the "join" method, to join strings
 const morgan = require('morgan');//information about web traffic
+const bodyParser = require('body-parser');
 
 const dbClient = require('./clients/db_client.js')
 
 const server = express(); //instance of express
 
 server.use(morgan('tiny')); //call Morgan utility
+server.use(bodyParser.json());
 
 server.use('/css', express.static(path.join(__dirname, '/public/')));//reference to static components in "public" folder
 server.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css/'))); //reference to static files downloaded as node modules.-CSS
@@ -26,18 +28,19 @@ server.get('/', (req, res) => { //get Method route
 server.get('/orders', (req, res) => {
     dbClient.getProducts().then(function (prods) {
         res.render('ordersForm', { products: prods });
-    })
+    });
 });
 
-server.post('/orders/new_order', (res, req) => {
-    let queryString = req.query;
-
-    /*db_client.order.save(queryString, function (err) {
-        if (err) res.json({status: 500, message: "There was an error trying to save the order"})
-    });*/
-
-    // if everyting ok, then returns to the orders form to create a new one
-    //res.render('ordersForm', {products: db_client.products.getAll()});
+server.post('/orders/new_order', (req, res) => {
+    dbClient.newOrder(req.body)
+        .then(function (response) {
+            console.log(response.message);
+            res.redirect(307, '/orders');
+        })
+        .catch(function (error) {
+            console.log(error.message);
+            res.redirect(307, '/orders');
+        });
 });
 
 // ###############    ORDERS    ###############
